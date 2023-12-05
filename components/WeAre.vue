b7bab9903c6480ac90d0b7ae898efe1641f48f28
“Mateo <mdiaz@kubo.co>
2023-12-05 17:23:44 -0500

    [Add] html skeleton of we-are info cards

--- components/WeAre.vue
+++ components/WeAre.vue
@@ -1,9 +1,73 @@
 <template>
     <div class="we-are">
-        <h1>WE ARE</h1>
+        <h2 class="title">
+            Somos Kubo una empresa de desarrollo de software
+        </h2>
+        <h3 class="title blue-color">
+            ¿Qué ofrecemos?
+        </h3>
+
+        <div class="content">
+            <div class="serviceItem">
+                <img
+                    width="347"
+                    height="276"
+                    src="/img/icons/services/cellphone.png"
+                    :alt="cellphone-icon"
+                />
+                <!-- card - text -->
+                <div class="cardText">
+                    <h3>Desarrollo de apps multiplataforma y nativas</h3>
+                    <p>
+                        Desarrollamos aplicaciones móviles que generan
+                        <b>ventajas competitivas en el mercado</b>
+                    </p>
+                </div>
+            </div>
+
+            <div class="serviceItem tw-justify-end reverse">
+                <img
+                    width="347"
+                    height="276"
+                    src="/img/icons/services/dessign.png"
+                    :alt="dessign-icon"
+                />
+
+                <!-- card - text -->
+                <div class="cardText">
+                    <h3>Diseño UX/ UI</h3>
+                    <p>
+                        Creamos 
+                        <b>ventajas competitivas en el mercado</b>
+                        para que los usuarios conecten con tu negocio
+
+                    </p>
+                </div>
+            </div>
+
+            <div class="serviceItem">
+                <img
+                    width="347"
+                    height="276"
+                    src="/img/icons/services/qa.png"
+                    :alt="cellphone-icon"
+                />
+
+                <!-- card - text -->
+                <div class="cardText">
+                    <h3>QA Testing</h3>
+                    <p>
+                        Hacemos las pruebas necesarias para que tu producto digital, salga con 
+                        <b>ventajas competitivas en el mercado</b>
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+export default {
 
+};
 </script>
