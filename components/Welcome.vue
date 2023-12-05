dc593a4c8af44390fabbeadb969460aa30a20359
“Mateo <mdiaz@kubo.co>
2023-12-05 16:40:06 -0500

    [Add] project-icons in welcome-component

--- components/Welcome.vue
+++ components/Welcome.vue
@@ -1,9 +1,32 @@
 <template>
-    <div class="welcome">
-        <h1>Welcome</h1>
-    </div>
+    <section class="welcome">
+        <h1>
+            Da el primer paso para crear tu app, 
+            <br/>
+            <b>Ellos ya lo hicieron:</b>
+        </h1>
+        <div class="project-icons">
+            <!-- Corrected the prop name in the v-for -->
+            <img v-for="icon in projectsList" :key="icon.altName"
+                :width="icon.width"
+                :height="icon.height"
+                loading="lazy"
+                :src="icon.src"
+                :alt="icon.altName"
+            >
+        </div>
+        <div class="business-form">
+        </div>
+    </section>
 </template>
 
 <script>
-
+export default {
+  props: {
+    projectsList: {
+      type: Array,
+      required: true,
+    },
+  },
+};
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,47 @@
 <script>
+export default {
+    data() {
+        return {
+            sucessfullProjects: [
+                {
+                    widht: 66,
+                    height: 74,
+                    src: '/img/icons/project-icons/laika.svg',
+                    altName: 'laika-icon'
+                },
+                {
+                    widht: 138,
+                    height: 39,
+                    src: '/img/icons/project-icons/gopass.svg',
+                    altName: 'gopass-icon'
+                },
+                {
+                    widht: 107,
+                    height: 105,
+                    src: '/img/icons/project-icons/gane.svg',
+                    altName: 'gane-icon'
+                },
+                {
+                    widht: 138,
+                    height: 78,
+                    src: '/img/icons/project-icons/maxxi.svg',
+                    altName: 'maxxi-icon'
+                },
+                {
+                    widht: 138,
+                    height: 32,
+                    src: '/img/icons/project-icons/banclic.svg',
+                    altName: 'banclic-icon'
+                },
+            ]
+        }
+    }
+}
 
 </script>
 <template>
     <div class="homePage">
-        <Welcome />
+        <Welcome :projectsList="sucessfullProjects"/>
         <WeAre />
         <Why />
         <Success />
